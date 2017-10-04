ebefd9fc9595c5e0c901247199b720b9bf06acb0
sohilpandya <sohilpandya@me.com>
2017-10-04 22:32:36 +0100

    worked on a simple search function that will return the basic resutls from algolia.

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React from "react";
 
-const Restaurants = ({numRestaurants, restaurants}) => {
-
+const Restaurants = ({ numRestaurants, restaurants, processingTime }) => {
+  const results = restaurants.map((restaurant, i) => {
     return (
-        <p>Restaurant</p>
-
-        
-    )
-}
+      <div>
+        <h3> {restaurant.name} </h3>
+        <p> rating {restaurant.rating} </p>
+        <p> {restaurant.review} reviews</p>
+        <p> {restaurant.type} reviews</p>
+        <p> {restaurant.address} </p>
+        <p> {restaurant.range}</p>
+      </div>
+    );
+  });
+  return (
+    <div>
+      {numRestaurants > 0 &&
+        `${numRestaurants} results found in ${(processingTime / 1000).toFixed(
+          3
+        )} seconds`}
+      {numRestaurants > 0 && results}
+      <p>Restaurant</p>
+    </div>
+  );
+};
 
 export default Restaurants;
